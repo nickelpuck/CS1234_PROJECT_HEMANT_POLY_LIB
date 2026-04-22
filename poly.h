2e5c2ba8e6fc1354972005f0d746a7cb9ecb4139
nickelpuck <cs25b031@smail.iitm.ac.in>
2026-04-23 00:01:43 +0530

    check

--- poly.h
+++ poly.h
@@ -1,61 +1,60 @@
 #ifndef POLY_H
 #define POLY_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
 
-// Polynomial represented as array of coefficients
-// coeffs[i] = coefficient of x^i
-// e.g., 3x^2 + 2x + 1 => coeffs = {1, 2, 3}, degree = 2
+//Poly rep as arr of coeff
+//coeffs[i]=coefft of x^i
+//e.g.,3x^2+2x+1 =>coeffs={1,2,3}, deg=2
 typedef struct {
     double *coeffs;
     int degree;
-} Poly;
+}Poly;
 
-// Create / Destroy
+//Create/Dest
 Poly *poly_create(int degree);
-Poly *poly_from_array(double *coeffs, int degree);
+Poly *poly_from_array(double *coeffs,int degree);
 Poly *poly_copy(const Poly *p);
-void  poly_destroy(Poly *p);
+void poly_destroy(Poly *p);
 
-// Display
+//Display
 void poly_print(const Poly *p);
-void poly_print_to_file(const Poly *p, FILE *f);
+void poly_print_to_file(const Poly *p,FILE *f);
 
-// Basic Arithmetic
-Poly *poly_add(const Poly *a, const Poly *b);
-Poly *poly_sub(const Poly *a, const Poly *b);
-Poly *poly_mul(const Poly *a, const Poly *b);
-Poly *poly_scalar_mul(const Poly *a, double scalar);
+//Basic Arithm
+Poly *poly_add(const Poly *a,const Poly *b);
+Poly *poly_sub(const Poly *a,const Poly *b);
+Poly *poly_mul(const Poly *a,const Poly *b);
+Poly *poly_scalar_mul(const Poly *a,double scalar);
 Poly *poly_negate(const Poly *a);
 
-// Calculus
+//Calculus
 Poly *poly_differentiate(const Poly *p);
-Poly *poly_integrate(const Poly *p, double constant);
+Poly *poly_integrate(const Poly *p,double constant);
 
-// Evaluation
-double poly_eval(const Poly *p, double x);
-double poly_eval_horner(const Poly *p, double x);
+//Evaln
+double poly_eval(const Poly *p,double x);
+double poly_eval_horner(const Poly *p,double x);
 
-// Properties
-int    poly_degree(const Poly *p);
+//Propert
+int poly_degree(const Poly *p);
 double poly_leading_coeff(const Poly *p);
-int    poly_is_zero(const Poly *p);
-int    poly_equals(const Poly *a, const Poly *b);
+int poly_is_zero(const Poly *p);
+int poly_equals(const Poly *a,const Poly *b);
 Poly  *poly_normalize(Poly *p);
 
-// Advanced
-Poly  *poly_compose(const Poly *f, const Poly *g);
-Poly  *poly_gcd(const Poly *a, const Poly *b);
-Poly  *poly_mod(const Poly *a, const Poly *b);
-Poly **poly_divmod(const Poly *a, const Poly *b);
-double poly_newton_root(const Poly *p, double x0, int max_iter, double tol);
-double *poly_roots_real(const Poly *p, int *count);
+//Adv
+Poly *poly_compose(const Poly *f,const Poly *g);
+Poly *poly_gcd(const Poly *a,const Poly *b);
+Poly *poly_mod(const Poly *a,const Poly *b);
+Poly **poly_divmod(const Poly *a,const Poly *b);
+double poly_newton_root(const Poly *p,double x0,int max_iter,double tol);
+double *poly_roots_real(const Poly *p,int *count);
 
-// File I/O
-int   poly_save(const Poly *p, const char *filename);
+//File I/O
+int poly_save(const Poly *p,const char *filename);
 Poly *poly_load(const char *filename);
 
 #endif
